08c93da5570417089bb96f8ff18f438c508ecd40
Lulu Htutt <lh543@cornell.edu>
2024-01-17 13:55:19 -0800

    frontend updates

--- sipscore-frontend/src/App.js
+++ sipscore-frontend/src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import banner from './photos/sip_score_banner.png';
+import React, { useState, useEffect } from "react";
+import Navigation from './components/NavigationBar/Navigation';
+import Header from './components/Header/Header';
+import SearchBar from './components/SearchBar/SearchBar';
+import CircularButton from './components/addButton';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [allDrinks, setAllDrinks] = useState([]);
+    const [drinks, setDrinks] = useState({
+        "name": "",
+        "serving_size": 0,
+        "picture": "",
+        "caffeine_amt": 0,
+        "seasonal": false,
+        "location": ""
+    })
+
+    useEffect(() => {
+        fetch("api/").then((res) => {
+            console.log(res);
+        }
+        );
+    }, [])
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Header />
+                <h1>SipScore</h1>
+                <Navigation></Navigation>
+                <SearchBar />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="photos/sip_score_banner.png"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Change test
+                </a>
+                <CircularButton />
+            </header>
+        </div>
+    );
 }
 
 export default App;
